b41361136825077763fc98789aad13c48dcd23f8
Talha Mansoor <talha131@gmail.com>
2012-12-30 19:55:57 +0500

    Adds method to create tags in Asana workspace

--- asana/asana.py
+++ asana/asana.py
@@ -342,3 +342,13 @@ class AsanaAPI(object):
         :param tag_id: id# of task
         """
         return self._asana('tags/%d/tasks' % tag_id)
+
+    def create_tag(self, tag, workspace):
+        """Create tag
+
+        :param tag_name : name of the tag to be created
+        :param workspace : id# of workspace in which tag is to be created
+        """
+        payload = {'name': tag, 'workspace': workspace}
+
+        return self._asana_post('tags', payload)
